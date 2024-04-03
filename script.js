6c752f4a398ee8bc3bf34499aa9f8d3652cd2772
Swagat kumar Barik <skbarik2300@gmail.com>
2024-04-03 22:42:28 +0530

    veronica created

---
+++ script.js
@@ -0,0 +1,47 @@
+const accessKey = "CaGQ7TxQtHbuc0_T3pRbSwKqHqc7_eoGWMhUHnE25w0";
+const searchForm = document.getElementById("search-form");
+const searchBox = document.getElementById("search-box");
+const searchResult = document.getElementById("search-result");
+const ShowMoreBtn = document.getElementById("show-more-btn");
+
+let keyword = "";
+let page = 1;
+
+async function searchImages() {
+  keyword = searchBox.value;
+  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  if (page == 1) {
+    searchResult.innerHTML = "";
+  }
+
+  const results = data.results;
+
+  results.map((result) => {
+    const image = document.createElement("img");
+    image.src = result.urls.small;
+    const imageLink = document.createElement("a");
+    imageLink.href = result.links.html;
+    imageLink.classList.add("card");
+    imageLink.target = "_blank";
+
+    imageLink.appendChild(image);
+    searchResult.appendChild(imageLink);
+  });
+
+  ShowMoreBtn.style.display = "block";
+}
+
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  page = 1;
+  searchImages();
+});
+
+ShowMoreBtn.addEventListener("click", () => {
+  page++;
+  searchImages();
+});
